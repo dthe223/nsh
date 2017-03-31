57ff107c6626494d983fdf209aa84d65b5560a91
Deavin Hester <deavinhester@uky.edu>
2017-03-31 16:41:22 -0400

    Revert "Added header and .cc file for program controls and added makefile"

--- Program_Control.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROGRAM_CONTROL_H
-#define PROGRAM_CONTROL_H
-
-#include <string>
-#include <vector>
-
-void scanner(std::string input, std::vector<std::string> &tokens);
-
-#endif
--- nsh.cc
+++ nsh.cc
@@ -1,21 +1,8 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include "Program_Control.h"
-//using namespace std;
 
 int main() {
 
-	std::string prompt = "nsh> ", userin = "";
-	std::vector<std::string> tokens;
-	std::cout << prompt;
-	std::getline(std::cin, userin);
-	while (userin != "done" && userin != "<control-D>") {
-		std::cout << "userin is \"" << userin << "\"\n";
-		scanner(userin, tokens);
-		std::cout << prompt;
-		std::getline(std::cin, userin);
-	}
+	
 
 	return 0;
 }
